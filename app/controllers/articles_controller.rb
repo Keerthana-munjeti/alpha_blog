07bb2f15e7eefd7baf4a5a3ecb7c95303dddd717
Keerthana <munjeti.saikeerthana@freshworks.com>
2021-08-31 00:25:50 -0700

    added update and edit methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,51 @@ class ArticlesController < ApplicationController
     def index
         @articles=Article.all
     end
+
+    def new
+        @article=Article.new
+    end
+
+    def edit
+        @article=Article.find(params[:id])
+        
+
+    end
+
+    # def update
+     #   @article=Article.find(params[:id])
+     #  if  @article.update(params.require(:article).permit(:title,:description))
+           
+      #      flash[:notice]= "The article is updated successfully"
+       #     redirect_to (@article)
+
+        #else
+         #  render 'edit'
+        
+       # end
+    #end
+
+    def update
+        @article = Article.find(params[:id])
+       if @article.update(title: params[:article][:title], description: params[:article][:description])
+          flash[:notice]= "The article is updated successfully"
+           redirect_to article_path(@article)
+       else
+         render "edit"
+      end
+    end
+      
+
+
+    def create
+        @article=Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+        redirect_to (@article)
+        flash[:notice]= "The article is created successfully"
+
+    else
+        render 'new'
+        
+        end
+    end
 end
